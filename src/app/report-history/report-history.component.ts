acd4b3f3e6994a1a9f7ab5c84f702c49e2003aa6
vincy <kellylove0012@gmail.com>
2023-10-30 01:22:12 +0800

    update

--- src/app/report-history/report-history.component.ts
+++ src/app/report-history/report-history.component.ts
@@ -1,22 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/@service/common.service';
+import { ActivatedRoute, Router, RouteConfigLoadEnd, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-report-history',
   templateUrl: './report-history.component.html',
   styleUrls: ['./report-history.component.css']
 })
-export class ReportHistoryComponent implements OnInit{
+export class ReportHistoryComponent implements OnInit {
+  reports: any[] = [];
 
-  constructor(private commonService: CommonService) { }
+  constructor(private router: Router, private http: HttpClient, private commonService: CommonService) { }
 
   ngOnInit(): void {
-    console.log(this.commonService.refreshToken());
     this.loadReportHistory();
   }
 
-  loadReportHistory(){
-    console.log('loadReportHistory');
+  loadReportHistory() {
+    const accessToken = localStorage.getItem('accessToken');
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${accessToken}`
+    });
+    const apiUrl = `http://127.0.0.1:8000/api/reports/history`;
+    this.http.post<any>(apiUrl, null, { headers })
+      .subscribe(response => {
+        console.log(response);
+        this.reports = response.data.reports;
+      },
+      (error) => {
+        localStorage.setItem('islogin', "0");
+        this.router.navigate(['/login']);
+        console.error('Login error', error);
+      });
   }
 
 }
